14b0a1d600b49772d49a6049047938ce058d1dbb
Gabriel_Freeze <gabriel.hili.20@um.edu.mt>
2021-04-20 20:46:26 +0200

    Init

---
+++ build/CMakeFiles/smash.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/smash.dir/smash.c.o"
+  "smash"
+  "smash.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/smash.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
