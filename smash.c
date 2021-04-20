95ea5ae0195d27c32173509e8c6e26928722de91
Gabriel_Freeze <gabriel.hili.20@um.edu.mt>
2021-04-20 21:13:51 +0200

    Added basic linenoise utility

--- smash.c
+++ smash.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <string.h>
+#include "includes/linenoise-master/linenoise.c"
 
 int main(int argc, char* argv)
 {
     printf("Hello World!!!!!\n");
+
+    char* input;
+    char* prompt = {"init> "};
+    char* exit_keyword = {"exit"};
+
+    while (((input = linenoise(prompt)) != NULL) && strcmp(input, exit_keyword))
+    {
+        if (input[0] != '\0' && input[0] != '/')
+        {
+            printf("%s\n",input);
+        }
+
+        free(input);
+    }
+
+
     return 0;
 }
